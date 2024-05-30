7659518ee9f3b11fbc12704dc88dabaa92fe3779
Pushkar Choudhary <96918138+Pushkariiit@users.noreply.github.com>
2024-05-31 04:49:02 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ import MainNav from './MainNav'
 const Header = () => {
   return (
     <div className='border-b-2 border-b-orange-500 py-6'>
-        <div className='container mx-auto flex justify-between items-center'>
-            <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>
-                Order&Eat
-            </Link>
-            <div className='md:hidden'><MobileNav/></div>
-            <div className='hidden md:block'><MainNav/></div>
-        </div>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>
+          Order&Eat
+        </Link>
+        <div className='md:hidden'><MobileNav /></div>
+        <div className='hidden md:block'><MainNav /></div>
+      </div>
     </div>
   )
 }
