bd622caac195e6be0272d2e43a20e0995786a3a8
Pushkar Choudhary <pushkariiitv@gmail.com>
2024-06-01 18:49:22 +0530

    user profile and restaurant page added

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/sonner";
 import { User } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
